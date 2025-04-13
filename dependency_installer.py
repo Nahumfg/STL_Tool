f1bf040a845e7ed277b991fe20affc523d2d68dc
Nahum F. Gómez <excalibur_965@hotmail.com>
2025-04-12 23:38:29 -0600

    Create dependency_installer.py

---
+++ dependency_installer.py
@@ -0,0 +1,22 @@
+import subprocess
+import sys
+
+def instalar_dependencias():
+    """
+    Instala las dependencias necesarias para el programa usando pip.
+    """
+    try:
+        # Lista de dependencias
+        dependencias = ["wxPython", "numpy", "pyvista", "numpy-stl"]
+
+        # Instalar cada dependencia
+        for paquete in dependencias:
+            print(f"Instalando {paquete}...")
+            subprocess.check_call([sys.executable, "-m", "pip", "install", paquete])
+        print("\nTodas las dependencias se instalaron correctamente.")
+
+    except Exception as e:
+        print(f"\nError al instalar las dependencias: {e}")
+
+if __name__ == "__main__":
+    instalar_dependencias()
